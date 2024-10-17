a540b393872f764b325cb909fe9640ab6dec8db0
Timothy Efraim Hotasi <timothyefraim35@gmail.com>
2024-10-17 14:14:56 +0700

    update exercise 2.19

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,83 +1,69 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-// TODO: Exercise 2.19
 const Countries = ({ list }) => {
+  const [refinedCountryList, setRefinedCountryList] = useState([])
 
-  // Old Approach
-  // const [listCountry, setListCountry] = useState(
-  //   list.map(country => {
-  //     const cData = {
-  //       commonName: country.name.common,
-  //       capital: country.capital,
-  //       area: country.area,
-  //       languages: country.languages,
-  //       flagImage: country.flags.png,
-  //       isShowed: false
-  //     }
-  //     console.log(cData)
-  //     return cData
-  //   })
-  // )
-
-  const filteredCountry = list.map(country => {
-    const cData = {
-      commonName: country.name.common,
-      capital: country.capital,
-      area: country.area,
-      languages: country.languages,
-      flagImage: country.flags.png,
-      isShowed: false
-    }
-    console.log(cData)
-    return cData
-  })
-
-  const [listCountry, setListCountry] = useState(filteredCountry)
-  
-  const handleShowDetail = (countryName) => {
-    console.log(list)
-    console.log(countryName)
-    console.log(listCountry)
-  }
-
-  if (list.length > 10) {
-    return <p>Too many matches, specify another filter</p>
-  } else if (list.length > 1) {
-    return ( 
-      list.map((c) => {
-        return (
-          <div key={c.name.common}>
-            {c.name.common} <button onClick={() => handleShowDetail(c.name.common)}>show</button>
-          </div>     
-        )
+  useEffect(() => {
+    setRefinedCountryList(
+      list.map(country => {
+        const countryData = {
+          commonName: country.name.common,
+          capital: country.capital,
+          area: country.area,
+          languages: country.languages,
+          flagImage: country.flags.png,
+          isShowed: false
+        }
+        return countryData
       })
     )
-  } else if (list.length === 1) {
-    const countryData = list[0]
-    const countryLanguages = []
-    
-    for (const lang in countryData.languages) {
-      countryLanguages.push(countryData.languages[lang])
+  }, [list])
+  
+  const handleShowDetail = (country) => {
+    const checkList = refinedCountryList
+
+    for (const c in checkList) {
+      if (refinedCountryList[c] === country) {
+        const newCountry = {
+          ...country,
+          isShowed: !country.isShowed
+        }
+        checkList[c] = newCountry
+      }
+    }
+
+    setRefinedCountryList(checkList)
+  }
+
+  if (refinedCountryList.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+  }
+
+  return refinedCountryList.map((c) => {
+    const countryLangs = []
+    for (const k in c.languages) {
+      countryLangs.push(c.languages[k])
     }
 
     return (
-      <div>
-        <h2>{countryData.name.common}</h2>
+      <div key={c.commonName}>
+        {c.commonName} <button onClick={() => handleShowDetail(c)}>{c.isShowed ? 'unshow' : 'show'}</button>
+        <div className={c.isShowed ? 'details' : 'details hide'}>
+          <h2>{c.commonName}</h2>
 
-        <p>capital {countryData.capital}</p>
-        <p>area {countryData.area}</p>
-
-        <h4>languages:</h4>
-
-        <ul>
-          {countryLanguages.map((lang) => <li key={lang}>{lang}</li>)}
-        </ul>
-        
-        <img src={countryData.flags.png} width={150}/>
+          <p>capital {c.capital}</p>
+          <p>area {c.area}</p>
+    
+          <h4>languages:</h4>             
+          <ul>
+            {countryLangs.map((lang) => <li key={lang}>{lang}</li>)}
+          </ul>
+          <img src={c.flagImage} width={150}/>        
+        </div>
       </div>
     )
-  }
+  })
+  
 }
-
 
 export default Countries
