d85e451fa29a3f68381d29d1f3056c483718d517
Timothy Efraim Hotasi <timothyefraim35@gmail.com>
2024-11-11 09:30:28 +0700

    exercise 4.19 done

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,40 +1,57 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getToken = (token) => {
+  if (token && token.startsWith('Bearer ')) {
+    return token.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response, next) => {
   const blogs = await Blog.find({})
     .populate('user', ['id', 'username', 'name'])
     .catch(error => next(error))
 
+  // const userToken = getToken(request.headers.authorization)
+
+  // const decodedToken = jwt.verify(userToken, process.env.SECRET)
+  // console.log(decodedToken)
+
+  // const user = await User.findById(decodedToken.id)
+  // console.log(user)
+
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  // console.log("BODY!", body)
+  const userToken = getToken(request.headers.authorization)
+  const decodedToken = jwt.verify(userToken, process.env.SECRET)
 
-  const userCount = await User.countDocuments()
-  const randomIndex = Math.floor(Math.random() * userCount)
-  const randomUser = await User.findOne().skip(randomIndex)
+  // We should handle token verification on jwt.verify return value.
+  // Then we need to specify id because jwt.verify could still return
+  // other values
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: randomUser
+    user: user._id // no need whole user object, just user._id
   })
 
-  try {
-    const savedBlog = await blog.save()
-    randomUser.blogs.push(savedBlog)
-    randomUser.save()
-    response.status(201).json(savedBlog)
-  } catch (e) {
-    return next(e)
-  }
-
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id) // also this, only its _id needed
+  await user.save()
+  
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
